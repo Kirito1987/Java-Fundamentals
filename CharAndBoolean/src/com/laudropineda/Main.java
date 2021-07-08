5de1adb2b3c73b28f525f083dcab7b67068f3a93
Kirito1987 <47379527+Kirito1987@users.noreply.github.com>
2021-07-07 22:30:04 -0500

    Add files via upload

---
+++ CharAndBoolean/src/com/laudropineda/Main.java
@@ -0,0 +1,20 @@
+package com.laudropineda;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //character primitive types
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        //boolean primitive type examples
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+
+    }
+}
